32048b667f3bbd261c67775fedf1bb120ce65718
Diana Badas <diana.badas@gmail.com>
2019-07-29 13:07:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const returnLastTwoDrivers = function (drivers) {
 };
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (fare) {
+  return function (value) {
+    return fare * value;
+  };
+};
